38539beda465d833f8d0753eef4ddfbb0cc2c728
putrapratamanst <putra@docotel.com>
2018-08-27 03:38:52 +0700

    test memory limit

--- wp-config.php
+++ wp-config.php
@@ -34,6 +34,9 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define ('WP_MEMORY_LIMIT','64m');
+
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
